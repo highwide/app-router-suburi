d2cdc247aa207fd842d8c42aaa4b6f0d07c73d5d
highwide <hochweit728@gmail.com>
2024-01-03 23:17:09 +0900

    ~ 12 yarikake

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,23 @@
+import { customers } from '@/app/lib/placeholder-data';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import Form from '@/app/ui/invoices/create-form';
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const id = params.id;
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Create Invoice',
+            href: `/dashboard/invoices/${id}/create`,
+            active: true,
+          },
+        ]}
+      />
+      <Form invoices={invoice} customers={customers} />
+    </main>
+  );
+}
